b3ff10b7c2c98a291a41671960d6f5383732ae70
anvs <anov.smtp@gmail.com>
2019-03-21 17:21:47 +0300

    Add MultiThreadTutorial module for test and tutorial.

---
+++ MultiThreadTutorial/src/main/java/com/anvs/MultiThreadTutorialApp.java
@@ -0,0 +1,13 @@
+package com.anvs;
+
+/**
+ * Hello world!
+ *
+ */
+public class MultiThreadTutorialApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MultiThreadTutorial/src/test/java/com/anvs/MultiThreadTutorialAppTest.java
@@ -0,0 +1,20 @@
+package com.anvs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MultiThreadTutorialAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
